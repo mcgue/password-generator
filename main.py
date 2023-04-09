405e1701932b9e6ca7f2fa63b6f4b2788d258964
mcgue <mcgu@protonmail.com>
2023-04-09 11:37:00 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Password Generator
+
+def generatepassword(length):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Here is your password with a length of {length}')
+
+# Main
+if __name__ == '__main__':
+    generatepassword(8)
+
