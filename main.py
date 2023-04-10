8baf4042bc39cd653a3c3f46673bd264bb533f38
mcgue <mcgu@protonmail.com>
2023-04-10 12:16:47 -0700

    add necessary modules and variables

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # Password Generator
 
+# Import modules
+import time
+
 def generatepassword(length):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Here is your password with a length of {length}')
+    print(f'Here is your password with a maximum length of {length}')
 
 # Main
 if __name__ == '__main__':
-    generatepassword(8)
+    print("This is a password generator. Answer the following questions:")
+    time.sleep(1)
+    max_length = input("What is the maximum length? ")
+    min_length = input("What is the minimum length? ")
+    generatepassword(max_length)
 
