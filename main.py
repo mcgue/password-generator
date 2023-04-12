e6b1ebe5e13dbad8fd1e3887004de976757d9475
mcgue <mcgu@protonmail.com>
2023-04-12 09:56:09 -0700

    expand collection of parameters

--- main.py
+++ main.py
@@ -2,16 +2,19 @@
 
 # Import modules
 import time
+import random
 
-def generatepassword(length):
+def generatepassword(max, min):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Here is your password with a maximum length of {length}')
+    print(f'Here is your password with a maximum length of {max}')
+    print(f'Here is your password with a minimum length of {min}')
 
 # Main
 if __name__ == '__main__':
-    print("This is a password generator. Answer the following questions:")
+    # Get parameters
+    print("This is a password generator. Answer the following questions...")
     time.sleep(1)
     max_length = input("What is the maximum length? ")
     min_length = input("What is the minimum length? ")
-    generatepassword(max_length)
+    generatepassword(max_length, min_length)
 
