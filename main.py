f399bf69905b5239c7053a11fd7bc0a622949ce9
mcgue <mcgu@protonmail.com>
2023-04-16 10:28:07 -0700

    create random selector

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import time
 import random
 import string as str
 
+# Create string with all acceptable characters
 acceptable_characters = str.ascii_letters + str.digits + str.punctuation
 
 def generatepassword(max, min):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Here is your password with a maximum length of {max}')
     print(f'Here is your password with a minimum length of {min}')
-
+    new_letter = random.choice(acceptable_characters)
+    print(new_letter)
 # Main
 if __name__ == '__main__':
     # Get parameters
@@ -20,4 +22,3 @@ if __name__ == '__main__':
     max_length = input("What is the maximum length? ")
     min_length = input("What is the minimum length? ")
     generatepassword(max_length, min_length)
-    print(acceptable_characters)
